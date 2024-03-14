3e7e70491fc96382bc3b44a6480089d35171ee1e
Niketh09 <77853738+Niketh09@users.noreply.github.com>
2024-03-14 13:00:14 +0530

    Update script_list.js
    removed comments and updated api endpoints

--- script_list.js
+++ script_list.js
@@ -1,43 +1,5 @@
-// const productList = document.getElementById('productList');
-
-//     // Function to fetch and render the list of products
-// function fetchProducts() {
-//     fetch('foodproducts.azurewebsites.net/api/listProducts') // Assuming this endpoint is configured on your backend server
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Failed to fetch products');
-//             }
-//             return response.json();
-//         })
-//         .then(products => {
-//             console.log(products)
-//             renderProductList(products);
-//         })
-//         .catch(error => {
-//             console.error('Error fetching products:', error);
-//         });
-// }
-
-// // Function to render the list of products
-// function renderProductList(products) {
-//     console.log(products)
-//     productList.innerHTML = ''; // Clear previous content
-
-//     products.forEach(product => {
-//         const listItem = document.createElement('li');
-//         listItem.textContent = `ID: ${product.name}, Name: ${product.description}, Price: ${product.price}`;
-//         productList.appendChild(listItem);
-//     });
-// }
-
-// // Fetch products when the page is loaded
-// fetchProducts();
-
 const productList = document.getElementById('product-list');
 
-// const products = [];
-
-// Assuming you have the product data in an array named 'products'
 function displayProducts(products) {
   productList.innerHTML = ""; // Clear existing table rows
   console.log(products)
@@ -54,19 +16,17 @@ function displayProducts(products) {
     const priceCell = document.createElement("td");
     priceCell.textContent = product.price;
 
-    // Append cells to the table row
     tableRow.appendChild(serialNumberCell);
     tableRow.appendChild(nameCell);
     tableRow.appendChild(descriptionCell);
     tableRow.appendChild(priceCell);
 
-    // Append the table row to the table body
     productList.appendChild(tableRow);
   });
 }
 
 function fetchProducts() {
-    fetch('/api/listProducts') // Assuming this endpoint is configured on your backend server
+    fetch('/foodproducts.azurewebsites.net/api/listProducts') 
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to fetch products');
